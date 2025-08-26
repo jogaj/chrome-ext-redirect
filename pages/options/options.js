dd70943f78073c1c1276fd265613fce4b2bc5fb4
Jonathan <jogaj_26@hotmail.com>
2025-08-26 04:58:54 +0000

    Fixed regExp for url format to include ipaddresses

--- pages/options/options.js
+++ pages/options/options.js
@@ -1,7 +1,7 @@
 import { SAVE_BTN, STATUS, STATUS_ICON, STATUS_CONTAINER, STATUS_MESSAGE, FORM, IP_TO_MATCH, REDIRECT_URL } from '../../shared.js';
 
 const REGEXP_IP = /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-const REGEXP_URL = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+const REGEXP_URL_OR_IP_SITE = /^(https?:\/\/)?((?:[0-9]{1,3}\.){3}[0-9]{1,3}|(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,})(:\d{1,5})?(\/.*)?$/;
 
 document.getElementById(STATUS_ICON).addEventListener('click', () => {
     const statusContainer = document.getElementById(STATUS_CONTAINER);
@@ -15,7 +15,7 @@ document.getElementById(SAVE_BTN).addEventListener('click', (e) => {
     const statusMessage = document.getElementById(STATUS_MESSAGE);
     const statusContainer = document.getElementById(STATUS_CONTAINER);
     statusContainer.classList.remove('hidden');
-    if (form.checkValidity() && isValid(IP_TO_MATCH, REGEXP_IP) && isValid(REDIRECT_URL, REGEXP_URL)) {
+    if (form.checkValidity() && isValid(IP_TO_MATCH, REGEXP_IP) && isValid(REDIRECT_URL, REGEXP_URL_OR_IP_SITE)) {
         const ipToMatch = document.getElementById(IP_TO_MATCH).value;
         const redirectUrl = document.getElementById(REDIRECT_URL).value;
         chrome.storage.sync.set({ ipToMatch, redirectUrl }, () => {
